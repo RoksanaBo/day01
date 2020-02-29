e01514f744204e1107e1028eb7fe13a7a7e48212
RoksanaBo <roksana85@hotmail.com>
2020-02-29 14:25:19 -0600

    modified StringPractice class

---
+++ src/day01/StringPractice.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+        String str = "Java is fun";
+        System.out.println(str);
+
+    }
+}
