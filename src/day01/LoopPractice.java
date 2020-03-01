a36519ad7ee0d16f9d14292738f3ae7cbece99c5
RoksanaBo <roksana85@hotmail.com>
2020-03-01 08:38:10 -0600

    update

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -9,6 +9,7 @@ public class LoopPractice {
         for (int i = 0; i <10 ; i+=2) {
             System.out.println("Counter = "+ i);
             System.out.println("Roksana");
+            System.out.println("git is fun");
         }
     }
 }
